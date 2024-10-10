9e1a4e97cc940cb6ad656aefb453b98346eb959b
Amanda Silva <61585589+amanndasilva@users.noreply.github.com>
2024-10-09 22:06:41 -0300

    mencoes
    Atividade de menções, onde o programa apenas imprime as informações

---
+++ Mencao/src/main/java/com/mycompany/mencao/Mencao.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mencao;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Mencao {
+
+    public static void main(String[] args) {
+        String mencoes[] = new String [4];
+        
+        for(int i = 0; i < 4; i++){
+            mencoes[0] = "MB";
+            mencoes[1] = "B";
+            mencoes[2] = "R";
+            mencoes[3] = "I";
+            
+            System.out.println("As mensções são: " + mencoes[i]);
+        }
+    }
+}
